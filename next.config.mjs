3284619709bf75b9792d078e53df08e14ec6343e
Blackbullet99 <isaacokanigbe@gmail.com>
2024-06-23 20:50:32 +0100

    STYLE: Add login page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from "path";
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(process.cwd(), "styles")],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
---
+++ src/components/navigation.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./ui/navbar/navbar";
+import Sidebar from "./ui/sidebar/sidebar";
+
+const Navigation = () => {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+    </>
+  );
+};
+
+export default Navigation;
